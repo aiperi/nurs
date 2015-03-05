57a61c167b635af3625c56e81cd84fa25e1d7ea3
aiperi <aiperi.akunova@gmail.com>
2015-03-05 14:46:58 +0600

    probnaya versiya

---
+++ hello.rb
@@ -0,0 +1,27 @@
+def greet(variable)
+=begin
+	(string) -> string
+	
+	this method has to accept string
+	and return string
+
+	>  greet("Azamat")
+	=> "Hello, Azamat!"
+=end
+	# write your code here
+end
+
+
+def hello
+=begin
+	() -> string
+
+	this method does not have to accept
+	any arguments and it has to return
+	string
+
+	>  hello()
+	=> "Hello!"
+=end
+	# write your code here
+end
